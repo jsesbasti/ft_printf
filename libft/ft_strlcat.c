96d547af6c6b762d391e886cfe080e1b7f94207b
Jose Manuel Sebastia Gomez <jsebasti@cbr4s5.42barcelona.com>
2022-09-17 19:15:34 +0200

    all

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -3,12 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 21:13:40 by marvin            #+#    #+#             */
-/*   Updated: 2022/09/16 21:13:40 by marvin           ###   ########.fr       */
+/*   Updated: 2022/09/17 19:14:17 by jsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
+#include <stdio.h>
 
+size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t i;
+	size_t j;
+
+	i = 0;
+	j = 0;
+	if (dstsize <= ft_strlen(dst))
+		return (dstsize + ft_strlen(src));
+	while (dst[i] != '\0')
+		i++;
+	while (j < dstsize)
+	{
+		if (src[j] != '\0')
+		{
+			dst[i] = src[j];
+			j++;
+			i++;
+		}
+	}
+	return (i);
+}
+
+int	main()
+{
+	char *d = "Hola buenas";
+	const char *s = "Que tal?";
+
+	printf("esto es el original del strlcat: %lu", ft_strlcat(d, s, 5));
+	return (0);
+}
