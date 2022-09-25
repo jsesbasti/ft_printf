6794124ae602a72aa6b63ce0bfac5ce8126eb550
jsesbasti <sebastiagomezjosemanuel@gmail.com>
2022-09-25 19:35:28 +0200

    _bonus

---
+++ libft/ft_lstadd_back_bonus.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/25 19:34:20 by jsebasti          #+#    #+#             */
+/*   Updated: 2022/09/25 19:34:20 by jsebasti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list *aux;
+
+	if (!new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	aux = ft_lstlast(*lst);
+	aux->next = new;
+}
---
+++ libft/ft_lstiter_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/25 19:33:31 by jsebasti          #+#    #+#             */
+/*   Updated: 2022/09/25 19:33:31 by jsebasti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*aux;
+
+	if (!lst)
+		return ;
+	while (lst)
+	{
+		aux = lst->next;
+		f(lst);
+		lst = aux;
+	}
+}
