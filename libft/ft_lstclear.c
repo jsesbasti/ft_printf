bc06159e2832643612bac97af9e8f4a50e9b7a94
jsesbasti <sebastiagomezjosemanuel@gmail.com>
2022-09-25 19:15:06 +0200

    lstclear

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -1,21 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/25 19:03:04 by jsebasti          #+#    #+#             */
-/*   Updated: 2022/09/25 19:03:04 by jsebasti         ###   ########.fr       */
+/*   Created: 2022/09/25 19:13:25 by jsebasti          #+#    #+#             */
+/*   Updated: 2022/09/25 19:13:25 by jsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list *lst, void (*del)(void*))
+void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
-	if (!lst)
+	t_list	*aux;
+
+	if (!*lst)
 		return ;
-	del(lst->content);
-	free(lst);
+	while (*lst)
+	{
+		aux = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = aux;
+	}
+	*lst = 0;
 }
