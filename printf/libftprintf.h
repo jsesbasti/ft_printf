a51459e692b007e9209802852c755bb9f50f12a1
Jose Manuel Sebastia Gomez <jsebasti@cbr2s2.42barcelona.com>
2022-09-27 17:47:44 +0200

    all

---
+++ printf/libftprintf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/27 16:06:39 by jsebasti          #+#    #+#             */
+/*   Updated: 2022/09/27 16:24:57 by jsebasti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+int		ft_printf(char const *, ...);
+char	char_conv(char const *, ...);
+
+#endif
