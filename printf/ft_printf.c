0247918528cc5294ac76240f405dddaf6275e152
Jose Manuel Sebastia Gomez <jsebasti@cbr6s4.42barcelona.com>
2022-09-27 21:37:50 +0200

    all

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,15 +6,13 @@
 /*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 16:02:45 by jsebasti          #+#    #+#             */
-/*   Updated: 2022/09/27 17:11:08 by jsebasti         ###   ########.fr       */
+/*   Updated: 2022/09/27 21:37:33 by jsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_printf(char const *conv, ...)
+int	ft_printf(char const *s, ...)
 {
-	
-	if (*conv == '%c')
-		char_conv(1, va_arg(ap, char))
+
 }
--- printf/libftprintf.h
+++ printf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 16:06:39 by jsebasti          #+#    #+#             */
-/*   Updated: 2022/09/27 16:24:57 by jsebasti         ###   ########.fr       */
+/*   Updated: 2022/09/27 21:37:31 by jsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <stdlib.h>
 # include <stdarg.h>
 
-int		ft_printf(char const *, ...);
-char	char_conv(char const *, ...);
+int		ft_printf(char const *s, ...);
+char	char_conv();
 
 #endif
