f82db6103aec2e98430c03dbe20de64f929198b8
Jose Manuel Sebastia Gomez <jsebasti@cbr13s5.42barcelona.com>
2022-10-09 19:37:41 +0200

    all

--- printf/src/ft_print_char.c
+++ printf/src/ft_print_char.c
@@ -6,7 +6,7 @@
 /*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 17:11:54 by jsebasti          #+#    #+#             */
-/*   Updated: 2022/09/29 12:28:28 by jsebasti         ###   ########.fr       */
+/*   Updated: 2022/09/29 19:45:43 by jsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 int	ft_print_char(char const c, int count)
 {
 	if (write(1, &c, 1) == -1)
+	{
 		count = -1;
+		return (-1);
+	}
 	else
 		count += 1;
 	return (count);
